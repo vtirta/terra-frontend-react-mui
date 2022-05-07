ec5cd9109d8c2d3ed6d1947f30c6e2fc64118ff9
Vic Tirta <vtirta@gmail.com>
2022-05-06 19:41:46 -0500

    Navbar + background

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,50 @@ import {QuerySample} from './components/QuerySample';
 import {SignBytesSample} from './components/SignBytesSample';
 import {SignSample} from './components/SignSample';
 import {TxSample} from './components/TxSample';
+
+import Container from '@mui/material/Container';
+import NavBar from './components/NavBar';
+
+import {ThemeProvider, createTheme} from '@mui/material/styles';
+import {purple} from '@mui/material/colors';
+
+import CssBaseline from '@mui/material/CssBaseline';
 import './App.css';
 
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            // Purple and green play nicely together.
+            main: purple[500],
+        },
+        background: {
+            default: "#303030"
+        }
+    },
+    typography: {
+        // In Chinese and Japanese the characters are usually larger,
+        // so a smaller fontsize may be appropriate.
+        fontSize: 18,
+    },
+});
+
 function App() {
-  return (
-    <div className="App">
-      <ConnectSample/>
-      <QuerySample/>
-      <TxSample/>
-      <SignSample/>
-      <SignBytesSample/>
-      <CW20TokensSample/>
-      <NetworkSample/>
-
-      <header className="App-header">
-
-      </header>
-
-    </div>
-  );
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline/>
+            <NavBar/>
+            <Container>
+                <ConnectSample/>
+                <QuerySample/>
+                <TxSample/>
+                <SignSample/>
+                <SignBytesSample/>
+                <CW20TokensSample/>
+                <NetworkSample/>
+            </Container>
+        </ThemeProvider>
+    );
 }
 
 export default App;
